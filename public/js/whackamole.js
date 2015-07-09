3a1fe2fc59dab62a6a3dad752bea43b5953dbd78
Alexandra Gutierrez <agutie95@yahoo.com>
2015-07-09 14:43:11 -0500

    edited content

--- public/js/whackamole.js
+++ public/js/whackamole.js
@@ -1,7 +1,9 @@
 'use strict';
 console.log('Welcome to Whack-a-Mole!');
 
-var computerSequence = [];
+var computerIndex = 0;
+var userIndex = 0;
+
 
 //activate start button
 $('#start').click(function(){
@@ -15,21 +17,40 @@ function getRandomSquare(){
     var randomMolehole =  Math.floor(Math.random() * 9);
     var moleholeToAnimate = moleholes[randomMolehole];
     var id = moleholeToAnimate.getAttribute('id');
-    animateSquare(id);
-    computerSequence.push(id);
-    console.log(computerSequence);
-
+    animateMolehole(id);
+    // computerSequence.push(id);
+    // console.log(animateSquare(id));
 };
 
-function animateSquare(id){
+function animateMolehole(id){
     $('#' + id).addClass('active');
     setTimeout(function(){
-        $('#' + id).removeClass('active');
+        fadeOut(id);
+        fadeTimeout();
     }, 1000);
 };
 
+function fadeOut(id){
+    $('#' + id).removeClass('active');
+};
+
+function fadeTimeout(){
+    setTimeout(getRandomSquare, 1000);
+}
+
 //click function to make mole disappear
-
-//add one point when mole is clicked
-
-//game over / start over
+$('.molehole').click(function(event){
+    var squareClicked = $(this).attr('id');
+    var randomSquare = $(this).hasClass('active');
+    console.log(squareClicked);
+    console.log(randomSquare);
+    if (randomSquare == true) {
+        fadeOut(squareClicked);
+        userIndex++;
+        $('#pointsCounter').val(userIndex);
+    }
+    if (userIndex >= 10){
+        alert ('YOU DEFEATED EMPEROR ZURG!');
+        location.reload(true);
+    }
+});
