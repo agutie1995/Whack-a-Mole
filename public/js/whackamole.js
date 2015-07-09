9a1542978db65a3d0a398ff83afc4351e02b8a20
Alexandra Gutierrez <agutie95@yahoo.com>
2015-07-09 16:07:51 -0500

    edited sound

--- public/js/whackamole.js
+++ public/js/whackamole.js
@@ -1,64 +1,61 @@
-'use strict';
-console.log('Welcome to Whack-a-Mole!');
+(function(){
 
-var computerIndex = 0;
-var userIndex = 0;
+    'use strict';
+    console.log('Welcome to Whack-a-Mole!');
+
+    var computerIndex = 0;
+    var userIndex = 0;
 
 
-//activate start button
-$('#start').click(function(){
-    console.log('Let\'s play!');
-    getRandomSquare();
-});
+    //activate start button
+    $('#start').click(function(){
+        console.log('Let\'s play!');
+        getRandomSquare();
+    });
 
-//get random molehole
-function getRandomSquare(){
-	var moleholes = $('.molehole');
-    var randomMolehole =  Math.floor(Math.random() * 9);
-    var moleholeToAnimate = moleholes[randomMolehole];
-    var id = moleholeToAnimate.getAttribute('id');
-    animateMolehole(id);
-};
+    //get random molehole
+    function getRandomSquare(){
+    	var moleholes = $('.molehole');
+        var randomMolehole =  Math.floor(Math.random() * 9);
+        var moleholeToAnimate = moleholes[randomMolehole];
+        var id = moleholeToAnimate.getAttribute('id');
+        animateMolehole(id);
+    };
 
-//animate molehole
-function animateMolehole(id){
-    $('#' + id).addClass('active');
-    setTimeout(function(){
-        fadeOut(id);
-        fadeTimeout();
-    }, 1000);
-};
+    //animate molehole
+    function animateMolehole(id){
+        $('#' + id).addClass('active');
+        setTimeout(function(){
+            fadeOut(id);
+            fadeTimeout();
+        }, 1000);
+    };
 
-function fadeOut(id){
-    $('#' + id).removeClass('active');
-};
+    function fadeOut(id){
+        $('#' + id).removeClass('active');
+    };
 
-//second setTimeout to call on animateMolehole
-function fadeTimeout(){
-    setTimeout(getRandomSquare, 1000);
-}
+    //second setTimeout to call on animateMolehole
+    function fadeTimeout(){
+        setTimeout(getRandomSquare, 1000);
+    };
 
-//sound
-function laser(){
-    $('#laser').play();
-}
-
-//click function to make mole disappear when user clicks
-    //add points
-    //end game
-$('.molehole').click(function(event){
-    var squareClicked = $(this).attr('id');
-    var randomSquare = $(this).hasClass('active');
-    console.log(squareClicked);
-    console.log(randomSquare);
-    if (randomSquare == true) {
-        fadeOut(squareClicked);
-        userIndex++;
-        $('#pointsCounter').val(userIndex);
-        laser();
-    }
-    if (userIndex >= 10){
-        alert ('YOU DEFEATED EMPEROR ZURG!');
-        location.reload(true);
-    }
-});
+    //click function to make mole disappear when user clicks
+        //add points
+        //end game
+    $('.molehole').click(function(event){
+        var squareClicked = $(this).attr('id');
+        var randomSquare = $(this).hasClass('active');
+        console.log(squareClicked);
+        console.log(randomSquare);
+        if (randomSquare == true) {
+            fadeOut(squareClicked);
+            userIndex++;
+            $('#pointsCounter').val(userIndex);
+        };
+        if (userIndex >= 5){
+            alert ('YOU DEFEATED EMPEROR ZURG!');
+            location.reload(true);
+        };
+    });
+})();
